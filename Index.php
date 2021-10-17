9c3aef942e07494bc7d0359784aeff0330283f64
Boyalfandy123 <lusiprih123@gmail.com>
2021-10-17 12:55:30 +0700

    mengedit file index.php

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Ng. Satria Utomo W.P.(155610024)</h3>"
+echo "<h3>Ng. Lusi Pri Hartini N.I.M.(205410059)</h3>"
 
 ?>
